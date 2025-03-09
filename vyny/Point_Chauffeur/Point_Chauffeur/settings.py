e41480c2e2515cfba24b2dcc30a9160ba2ba3299
mohamed <mohamed.ouattara@epitech.eu>
2025-03-09 09:24:08 +0000

    Optimisation de requirements.txt

--- vyny/Point_Chauffeur/Point_Chauffeur/settings.py
+++ vyny/Point_Chauffeur/Point_Chauffeur/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cd)g_397bv0vbt%g29tx5$+7%ofgk8ou51bd3#l8@c*m#)^q6%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
